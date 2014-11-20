3eda43d0b458a230ac368a728b0b79d2c93a5978
wutongyudemo <root@wutongyu.info>
2014-11-20 22:10:08 +0800

    wutongyu-tools-box
    梧桐雨工具箱

---
+++ js/options.js
@@ -0,0 +1,7 @@
+var interval = localStorage.interval;
+interval = interval?interval:'2'; //默认设置2小时
+document.getElementById('interval').value = interval;
+document.getElementById('save').onclick = function(){
+    localStorage.interval = document.getElementById('interval').value;
+    alert('保存成功。请重启浏览器生效！');
+}
